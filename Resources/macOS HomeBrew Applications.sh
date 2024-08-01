74c5b7acdc893de604ab1382942b157003740484
MOTHmusic <112948605+MOTHmusic@users.noreply.github.com>
2024-08-01 19:13:14 +0000

    Create macOS HomeBrew Applications 0.0.1.sh
    Automation script alpha test

---
+++ Resources/macOS HomeBrew Applications.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# sh "macOS HomeBrew Applications.sh"
+# macOS Bash Script
+echo "Installing HomeBrew Applications..."
+START_TIME=$(date +%s)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+wait
+brew analytics off
+wait
+brew install --cask vscodium libreoffice librewolf tailscale dupeguru steam syncthing todoist qbittorrent gimp parsec modrinth discord proton-mail protonvpn proton-drive proton-pass grandperspective
+wait
+END_TIME=$(date +%s)
+ELAPSED_TIME=$((END_TIME - START_TIME))
+clear
+echo "Installation complete (${ELAPSED_TIME}s)"
